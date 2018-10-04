f1907145e0f7bcd5e289e3560a63392ae910875d
Alexey Kalayda <akalaida@stream.ru>
2018-10-04 23:54:19 +0300

    Kotlin style

--- src/kotlin/com/itgrx/lib/mvp/module/ModuleContext.kt
+++ src/kotlin/com/itgrx/lib/mvp/module/ModuleContext.kt
@@ -4,10 +4,4 @@ package com.itgrx.lib.mvp.module
  * Created by Алексей Калайда on 10.10.2017.
  */
 
-interface ModuleContext {
-
-    fun getModuleContainer(): ModuleContainer
-    fun getModuleManager(): ModuleManager?
-    fun getExtraManager(name: String): Any?
-
-}
+open class ModuleContext(val moduleContainer: ModuleContainer?, val moduleManager: ModuleManager?)
